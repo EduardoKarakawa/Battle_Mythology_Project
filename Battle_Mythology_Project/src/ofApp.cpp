904c1658dc16d769a7ef30c16db72d5a840c5c5a
EduardoKarakawa <eduardo.karakawa@hotmail.com>
2016-09-20 06:57:47 -0300

    Colisao funcionando

--- Battle_Mythology_Project/src/ofApp.cpp
+++ Battle_Mythology_Project/src/ofApp.cpp
@@ -2,78 +2,108 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	//Pegando o tempo de execução do jogo
+	before = ofGetElapsedTimef();
+
 
 	//Definindo posição do mundo
-	Mundo.x = 0;
-	Mundo.y = 0;
+	Mundo.x = 640 - 520;
+	Mundo.y = -1055+360;
 
 
 	//Inicializando o terreno
-	terreno.sprite.loadImage("cenario/terreno.png");
+	terreno.sprite.loadImage("cenario/terrain.png");
 	terreno.pos.x = Mundo.x;
 	terreno.pos.y = Mundo.y;
 
 
-	//Iniciando a decoração Pilar
-	decPilar.sprite.loadImage("cenario/pilar.png");
-	decPilar.pos.x = 500;
-	decPilar.pos.y = 500;
-	decPilar.pivo = 260;
+	//Inicializando as decorações
+	//------Dec Barreira primeiro plano--------
+	decoracao[0].sprite.loadImage("cenario/dec_01.png");
+	decoracao[0].pivo = decoracao[0].sprite.getHeight() / 2;
+	decoracao[0].pos.x = Mundo.x + terreno.sprite.getWidth() / 2 - decoracao[0].sprite.getWidth() / 2 - 30;
+	decoracao[0].pos.y = Mundo.y + 618 - decoracao[0].pivo;
 
 
-	
+	//Inicializando pontos de colisao
+	IniciaParedes(&paredes[0],  0, 953, 355, 246);
+	paredes[0].teste.loadImage("cenario/terreno.png");
+	paredes[0].teste.resize(355, 246);
+	/*IniciaParedes(&paredes[1], Mundo.x + 686, Mundo.y + 953, 355, 246);
+	IniciaParedes(&paredes[2], Mundo.x + 321, Mundo.y + 1172, 400, 64);
+	IniciaParedes(&paredes[3], Mundo.x + 99, Mundo.y + 765, 355, 216);
+	IniciaParedes(&paredes[4], Mundo.x + 587, Mundo.y + 765, 355, 216);
+	IniciaParedes(&paredes[5], Mundo.x + 0, Mundo.y + 629, 407, 188);
+	IniciaParedes(&paredes[6], Mundo.x + 643, Mundo.y + 629, 398, 188);
+	IniciaParedes(&paredes[7], Mundo.x + 0, 0, Mundo.y + 178, 678);*/
+
 	//Iniciando o Player
-	player.Frame = 0;
-	player.posicao.x = 640;
-	player.posicao.y = 360;
-
-
+	player.velocidadeAnimacao = 0.5f;
+	player.posicao.x = Mundo.x + 520;
+	player.posicao.y = Mundo.y + 1055;
+	player.altura = 76;
+	player.largura = 76;
+	player.indexY = 3;	//Indica posição em Y do frame que sera exibido
+	player.indexX = 0;	//Indica posição em X do frame que sera exibido
 	
 }
 
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+	//Calculando o tempo de troca de frame
+	gameTime = ofGetElapsedTimef() - before;
+	before = ofGetElapsedTimef();
 
-	
+	player.frameTime += gameTime;
 
+	//if (player.frameTime >= player.velocidadeAnimacao) {
+	//	MudaFrame(&player);
+	//}
+
+	//Verificando colizao
+	ColisaoPlayer(&player, &paredes[0], &Mundo, &keys);
+	/*ColisaoPlayer(&player, &paredes[1], &Mundo);
+	ColisaoPlayer(&player, &paredes[2], &Mundo);
+	ColisaoPlayer(&player, &paredes[3], &Mundo);
+	ColisaoPlayer(&player, &paredes[4], &Mundo);
+	ColisaoPlayer(&player, &paredes[5], &Mundo);
+	ColisaoPlayer(&player, &paredes[6], &Mundo);
+	ColisaoPlayer(&player, &paredes[7], &Mundo);*/
 
 	//Verifica a direção para onde o player esta andando
 	SeeDirection(&player, &keys);
-	
+
 	//Verifica a ação que o player vai fazer
 	SeeAction(&player, &keys);
 
-	//Verificando se o player esta colidindo com algo
-	ColisaoPlayer(&player, &decPilar, &Mundo, &keys);
-	
 	//Movimenta o player
-	MovPlayer(&player, &keys, &Mundo);
+	MovPlayer(&player, &keys, &Mundo, gameTime);
 
 
 
 
-	std::cout << "Player x " << player.posicao.x << " y " << player.posicao.y << "VelX " << player.VelX << " y " << player.VelY << std::endl;
+	std::cout	<< "Player x " << player.posicao.x << " y " << player.posicao.y << " VelX " << player.VelX << " VelY " << player.VelY
+				<< " Time " << gameTime << " Before " << before << std::endl;
 }
-
 
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	player.FPS++;
-
-	//Muda os frames e as animações
-	Animation(&player, &keys);
 
 	//Desenhando o terreno
 	terreno.sprite.draw(Mundo.x, Mundo.y);
 
+	//Desenhando a decoração
+	decoracao[0].sprite.draw(Mundo.x + terreno.sprite.getWidth() / 2 - decoracao[0].sprite.getWidth()/2 - 30, Mundo.y + 618 - decoracao[0].pivo);
+
 	//Desenhando o player
-	player.Sprite.draw(640 - (player.Sprite.getWidth() / 2), 360 - (player.Sprite.getHeight() / 2));
+	//Muda os frames e as animações
+	DesenhaAnimation(&player, &keys);
 
+	paredes[0].teste.draw(paredes[0].pos.x + Mundo.x, paredes[0].pos.y + Mundo.y);
 
-	//Desenhando Pilar
-	decPilar.sprite.draw(Mundo.x + decPilar.pos.x - (decPilar.sprite.getWidth() / 2), Mundo.y + decPilar.pos.y - decPilar.pivo);
+	std::cout << " XX " << paredes[0].pos.x + Mundo.x << " YY " << paredes[0].pos.y + Mundo.y;
 
 }
 
--- Battle_Mythology_Project/src/ofApp.h
+++ Battle_Mythology_Project/src/ofApp.h
@@ -3,9 +3,9 @@
 #include "ofMain.h"
 #include <string>
 
-#define MAXSPEED 10
-#define PLAYERATRITO 0.1f
-#define PLAYERSPEED 1.f
+#define MAXSPEED 500
+#define PLAYERATRITO 23.f
+#define PLAYERSPEED 50.f
 
 struct PlayerType {
 	ofImage Sprite;
@@ -13,8 +13,10 @@ struct PlayerType {
 	int Vida;
 	int Direcao = 1;
 	char Acao[9] = "idle";
-	int Frame, FPS;
 	float VelX, VelY;
+	float frameTime, velocidadeAnimacao;
+	int indexX, indexY, altura, largura;
+
 	bool Vivo;
 };
 
@@ -33,18 +35,28 @@ struct KeyInput {
 struct BackgroundType {
 	ofPoint pos;
 	ofImage sprite;
+	float tam;
 	int pivo;
 };
 
-
+struct HitBoxType {
+	ofPoint pos;
+	int tamanhoX;
+	int tamanhoY;
+	ofImage teste;
+};
 
 class ofApp : public ofBaseApp{
 
 	ofPoint Mundo;
 	BackgroundType terreno;
-	BackgroundType decPilar;
+	BackgroundType decoracao[5];
+	HitBoxType paredes[8];
 	KeyInput keys;
 	PlayerType player;
+
+	float before;
+	float gameTime;
 	
 	public:
 		void setup();
@@ -68,42 +80,42 @@ class ofApp : public ofBaseApp{
 		void SeeDirection(PlayerType *ply, KeyInput *tecla) {
 		// Funcao para verificar se para qual direcao o player esta indo:
 			/*				  	
-						   2   3   4	 
+						   1   2   3	 
 							\  |  /
-						1 --- key --- 5
+						0 --- key --- 4
 						    /  |  \
-						   8   7   6
+						   7   6   5
 			*/
 
 
 			int aux = ply->Direcao;	//salva a direcao anterior, para continuar na mesma posicao se nao tiver apertado nada.
-			ply->Direcao = 0;		//zera a direcao para iniciar uma nova contagem
+			ply->Direcao = -1;		//zera a direcao para iniciar uma nova contagem
 
 			if (tecla->Left) {
-				ply->Direcao = 1;
+				ply->Direcao = 0;
 			}
 			if (tecla->Up) {
-				ply->Direcao = 3;
-			}
-			if (tecla->Right) {
-				ply->Direcao = 5;
-			}
-			if (tecla->Down) {
-				ply->Direcao = 7;
-			}
-			if (tecla->Left && tecla->Up) {
 				ply->Direcao = 2;
 			}
-			if (tecla->Right && tecla->Up) {
+			if (tecla->Right) {
 				ply->Direcao = 4;
 			}
-			if (tecla->Right && tecla->Down) {
+			if (tecla->Down) {
 				ply->Direcao = 6;
 			}
-			if (tecla->Left && tecla->Down) {
-				ply->Direcao = 8;
+			if (tecla->Left && tecla->Up) {
+				ply->Direcao = 1;
 			}
-			if ((ply->Direcao == 0) || (ply->Direcao > 11)) {	//Se nenhuma tecla tiver sido precionada ou a soma das teclas 
+			if (tecla->Right && tecla->Up) {
+				ply->Direcao = 3;
+			}
+			if (tecla->Right && tecla->Down) {
+				ply->Direcao = 5;
+			}
+			if (tecla->Left && tecla->Down) {
+				ply->Direcao = 7;
+			}
+			if ((ply->Direcao == -1) || (ply->Direcao > 11)) {	//Se nenhuma tecla tiver sido precionada ou a soma das teclas 
 				ply->Direcao = aux;								//for maior que 11, ply->Direcao recebe a direcao anterior.
 			}
 		}
@@ -127,22 +139,21 @@ class ofApp : public ofBaseApp{
 		}
 
 
-		void Animation(PlayerType *ply, KeyInput *tecla) {
+		void DesenhaAnimation(PlayerType *ply, KeyInput *tecla) {
 		//Funcao que controla o load dos sprites do player, conforme a Acao e Direcao, alem de mudar os frames.
 			
-			if (ply->FPS >= 10) {	//ply tem uma var que conta os fps, aki se ela for menor igual a 10
-				ply->FPS = 0;		//ela eh zerada e a variavel frame soma 1.
-				ply->Frame++;
-			}
-			if (ply->Frame > 3) {	//aki ocorre o mesmo que com o fps, mas com os frames, quando Frame passar de 3
-				ply->Frame = 0;		//o Frame eh zerado.
-			}
-
 			//Aki eh onde sera juntado a variavel Acao, Direcao e Frame, para poder carregar os frames de acordo com o 
 			//o jogador esta fazendo.
 			char anim[50];	//tudo sera juntado nessa variavel
-			snprintf(anim, sizeof(anim), "players/%s/%d/%d.png", ply->Acao,ply->Direcao, 0); //juntando tudo
-			ply->Sprite.loadImage(anim);
+			
+			snprintf(anim, sizeof(anim), "players/%s.png", ply->Acao); //juntando tudo
+
+			ply->Sprite.loadImage(anim);	//Carrega a imagem
+			ply->indexY = ply->Direcao;		//Muda o indexY de acordo com a direção pra onde o player ta andando
+			ply->frameTime = 0;
+
+			//Desenha o player na tela
+			ply->Sprite.drawSubsection(640 - ply->largura/2 , 360 - ply->altura/2, ply->largura, ply->altura, ply->indexX *ply->largura, ply->indexY * ply->altura);
 		}
 		
 
@@ -163,24 +174,24 @@ class ofApp : public ofBaseApp{
 		}
 
 
-		void MovPlayer(PlayerType *ply, KeyInput *tecla, ofPoint *mnd) {
+		void MovPlayer(PlayerType *ply, KeyInput *tecla, ofPoint *mnd, float tempo) {
 
 			//Atualiza da velocidade que do player
 			SpeedPlayer(ply, tecla);
 
 			//Subtrai a velocidade de Mundo para o mundo ser desenhado no lugar certo
-				mnd->x -= ply->VelX;
-				mnd->y -= ply->VelY;
+				mnd->x -= ply->VelX * tempo;
+				mnd->y -= ply->VelY * tempo;
 			//Soma na posição do player para ele andar no mundo
-				ply->posicao.x += ply->VelX;
-				ply->posicao.y += ply->VelY;
+				ply->posicao.x += ply->VelX * tempo;
+				ply->posicao.y += ply->VelY * tempo;
 			//Reduz a velocidade gradualmente do player quando ele nao estiver andando
 				if (ply->Acao == "idle");
-					DecreVelo(ply);
+				AtritoVelocidade(ply);
 		}
 
 
-		void DecreVelo(PlayerType *ply) {
+		void AtritoVelocidade(PlayerType *ply) {
 			//Função de atrito do personagem, ela faz o personagem ir reduzindo de velocidade gradualmente quando estiver parando de andar
 			if (ply->VelX > 0) {
 				ply->VelX -= PLAYERATRITO;
@@ -209,17 +220,32 @@ class ofApp : public ofBaseApp{
 		}
 
 
-		void ColisaoPlayer(PlayerType *ply, BackgroundType *obstaculo, ofPoint *mnd, KeyInput *tecla) {
+		void ColisaoPlayer(PlayerType *ply, HitBoxType *obstaculo, ofPoint *mnd, KeyInput *tecla) {
+			float tam = ply->Sprite.getWidth() / 2;
 
-			//Calcula a distancia entre os dois objetos
-			float distancia = obstaculo->sprite.getWidth() / 2 + ply->Sprite.getWidth() / 2;
+			
+			if ((obstaculo->pos.x + mnd->x <= ply->posicao.x) && ((obstaculo->pos.x + mnd->x + obstaculo->tamanhoX) >= ply->posicao.x)) {
+				tecla->Left_Disable = -tecla->Left_Disable;
+				tecla->Right_Disable = -tecla->Right_Disable;
+				ply->VelX = -ply->VelX;
+			}
+			
+			if (((ply->posicao.y) >= (obstaculo->pos.y + mnd->y)) && ((ply->posicao.y + tam) <= (obstaculo->pos.y + obstaculo->tamanhoY + mnd->y))) {
+				tecla->Up_Disable = -tecla->Up_Disable;
+				tecla->Down_Disable = -tecla->Down_Disable;
+				ply->VelY = -ply->VelY;
+			}
 
-			//Verifica se o player esta encostando ou embaixo do objeto
-			if (obstaculo->pos.distance(ply->posicao) < distancia) {
-				
+
+			////Calcula a distancia entre os dois objetos
+			//float distancia = obstaculo->tam + ply->Sprite.getWidth() / 2;
+
+
+			////Verifica se o player esta encostando ou embaixo do objeto
 				/*Desabilida os inputs de direção para impedir o que o jogador ande para baixo do
 				objeto, mas deixa liberado as teclas que tirao ele de baixo do objeto*/
-				if (obstaculo->pos.x < ply->posicao.x) {
+
+				/*if (obstaculo->pos.x < ply->posicao.x) {
 					if(ply->VelX < 0)
 						ply->VelX = 0;
 					tecla->Left_Disable = true;
@@ -246,16 +272,29 @@ class ofApp : public ofBaseApp{
 					tecla->Down_Disable = true;
 					tecla->Down = false;
 				}
+		
 			}
 
 
-			/*Caso o player não estaja encostando em nada, nenhuma tecla de movimentação é travada*/
+			/*Caso o player não estaja encostando em nada, nenhuma tecla de movimentação é travada
 			else {
 				tecla->Up_Disable = false;
 				tecla->Down_Disable = false;
 				tecla->Left_Disable = false;
 				tecla->Right_Disable = false;
-			}
+			}*/
+		}
+
+
+		void MudaFrame(PlayerType *ply) {
+			ply->indexX = (ply->indexX + 1) % ply->largura;
+		}
+
+		void IniciaParedes(HitBoxType *pared, int x, int y, int tamx, int tamy) {
+			pared->pos.x = x;
+			pared->pos.y = y;
+			pared->tamanhoX = tamx;
+			pared->tamanhoY = tamy;
 		}
 
 };
